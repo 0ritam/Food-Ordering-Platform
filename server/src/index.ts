4c524511e14fa601a63b04363e1146a5856fd967
0ritam <mujritam085@gmail.com>
2025-11-06 18:09:48 +0530

    feat: User Auth Implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { type Request, type Response, type NextFunction } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
